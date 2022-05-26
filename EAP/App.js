7f0a6003b48ea1afd8c87fccb5d031598c8f270f
Mya-Junkin <31495447+Mya-Junkin@users.noreply.github.com>
2022-05-26 14:59:26 -0400

    test of git through visual studio

--- EAP/App.js
+++ EAP/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>One small step for man...</Text>
+      <Text>One small step for man..</Text>
       <StatusBar style="auto" />
     </View>
   );
