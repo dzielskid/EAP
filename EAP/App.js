139dfbdd078fac45c01d1a9a65b775a9d95ef822
Mya-Junkin <31495447+Mya-Junkin@users.noreply.github.com>
2022-07-21 13:03:10 -0400

    Added Javascript files for each screen + screen nav install
    Created new folder container all screens (besides Main Screen which is App.js). Installed navigation for screens using command 'npm i @react-navigation/drawer'

--- EAP/App.js
+++ EAP/App.js
@@ -1,10 +1,36 @@
+/*=============================================================================
+ |   File Purpose:  Starts app as whole
+ |
+ |       Author:  
+ |     Language:  JavaScript
+ |                      NAME OF THE COMPILER USED TO COMPILE IT WHEN IT
+ |                      WAS TESTED
+ |   To Compile:  in terminal run 'npm start' command
+ |
+ +-----------------------------------------------------------------------------
+ |
+ |  Description:  DESCRIBE THE PROBLEM THAT WAS WRITTEN TO
+ |      SOLVE.
+ |
+ |        Input:  NONE
+ |
+ |       Output:  DESCRIBE THE OUTPUT PRODUCED.
+ |
+ |    Algorithm:  
+ |
+ |   Required Features Not Included:  Start on Main Screen
+ |
+ |   Known Bugs:  
+ |
+ *===========================================================================*/
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>One small step for man..</Text>
+      <Text>One small step for man...</Text>
       <StatusBar style="auto" />
     </View>
   );
