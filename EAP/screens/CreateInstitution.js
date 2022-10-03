2a61acf4e62c527458b5f4a2c299e70a1d9c7580
Mya-Junkin <mya.junkin@gmail.com>
2022-10-03 16:50:51 -0400

    merging createInstitution

--- EAP/screens/CreateInstitution.js
+++ EAP/screens/CreateInstitution.js
@@ -1,14 +1,98 @@
-import * as React from 'react';
-import { View, Text, Button  } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, Button, TextInput, Pressable , Alert } from 'react-native';
 
-function CreateInstitutionScreen() {
+function CreateInstitutionScreen({ navigation }) {
+    const [OrganizationName, setOrganizationName] = useState('WVU soccer Faculty');
+    const [OrganizationAddress, setOrganizationAddress] = useState('13555 Peabody st');
+    const [city, setCity] = useState('Morgantown');
+    const [state, setState] = useState('WV');
+    const [zip, setzip] = useState('26505');
+    const [AdminName, setAdminName] = useState('Dale');
+    const [AdminEmail, setAdminEmail] = useState('Dale.Dzielski@mail.wvu.edu');
+    //const { onPress, title = 'Submit' } = props;
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        <View style={styles.container}
         //Add button to "invite" that returns to the account screen
         >
-            <Text>Create Institution Screen</Text>
+            <Text>Organization Name:</Text>
+            <TextInput
+                placeholder=''
+                style={styles.input}
+                onChangeText={(value) => setOrganizationName(value)} />
+
+            <Text>Organization Address:</Text>
+            <TextInput
+                placeholder=''
+                style={styles.input}
+                onChangeText={(value) => setOrganizationAddress(value)} />
+
+            <Text>city:</Text>
+            <TextInput
+                placeholder=''
+                style={styles.input}
+                onChangeText={(value) => setCity(value)} />
+
+            <Text>state:</Text>
+            <TextInput
+                placeholder=''
+                style={styles.input}
+                onChangeText={(value) => setState(value)} />
+
+            <Text>zip:</Text>
+            <TextInput
+                placeholder=''
+                style={styles.input}
+                onChangeText={(value) => setzip(value)} />
+
+            <Text>AdminName:</Text>
+            <TextInput
+                placeholder=''
+                style={styles.input}
+                onChangeText={(value) => setAdminName(value)} />
+
+            <Text>AdminEmail:</Text>
+            <TextInput
+                placeholder=''
+                style={styles.input}
+                onChangeText={(value) => setAdminEmail(value)} />
+
+            <Button
+                title='Submit'
+                onPress={() => {
+                    Alert.alert('', 'New Institution Request Submitted')
+                    navigation.goBack()
+                }}
+            />
+
+            <Text style={styles.result}>name: {OrganizationName}, address: {OrganizationAddress}, city: {city}, state: {state}, zip:{zip}, AdminName:{AdminName}, AdminEmail: {AdminEmail}</Text>
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#777',
+        padding: 8,
+        margin: 10,
+        width: 200,
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'blue',
+    }
+});
 
 export { CreateInstitutionScreen };
