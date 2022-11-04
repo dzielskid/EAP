a5697c2ccfbbf9f812833c25241a3207eb625b24
Befikadu Kebeede <bk0023@mix.wvu.edu>
2022-11-04 13:10:05 -0400

    call911

---
+++ EAP/components/call911.js
@@ -0,0 +1,11 @@
+import call from 'react-native-phone-call'
+
+const args = {
+  number: '9093900003', // String value with the number to call
+  prompt: false, // Optional boolean property. Determines if the user should be prompted prior to the call 
+  skipCanOpen: true // Skip the canOpenURL check
+  
+  
+}
+
+call(args).catch(console.error)
