8fb38ab12286560d7612e2a696caf8d877fbe6e0
Befikadu Kebeede <bk0023@mix.wvu.edu>
2022-11-04 13:50:40 -0400

    antested

--- EAP/screens/CreateEditorScreen.js
+++ EAP/screens/CreateEditorScreen.js
@@ -1,18 +1,48 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput,button,Pressable } from 'react-native';
 
 function CreateEditorScreen({ navigation }) {
+    const [Editor, setEditorName] = useState('');
+    const [Editoremail, setEditorEmail] = useState('13555 Peabody st');
+    const [Organization, setOrganizationName]=useState('Morgantown');
+        
+    const {onPress,title = 'Invite'}=props;
+      
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-        //Add button to "invite" that returns to the account screen
-        >
-            <Text>Create Editor Screen</Text>
-            <Button
-                title="Invite"
-                onPress={() => navigation.goBack()}
-            />
-        </View>
-    );
-}
-
+        
+      
+        
+          <View style={styles.container}>
+      
+            <Text>Editor Name:</Text>
+            <TextInput 
+              placeholder='' 
+              style={styles.input}
+              onChangeText={(value) => setEditorName(value)} />
+      
+            <Text>Editor Email:</Text>
+            <TextInput 
+              placeholder='' 
+              style={styles.input}
+              onChangeText={(value) => setEditorEmail(value)} />
+      
+      <Text>Organization Name:</Text>
+            <TextInput 
+              placeholder='' 
+              style={styles.input}
+              onChangeText={(value) => setOrganizationName(value)} />
+      
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>{title}</Text>
+      </Pressable>
+      
+            <Text style={styles.result}>name: {EditorName}, Email: {Editoremail}, Organization: {OrganizationName}</Text>
+            
+          </View>
+        );
+      
+      }
+      
+               
 export { CreateEditorScreen };
